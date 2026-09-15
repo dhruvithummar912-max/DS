9c0a6cafa9510d4a352e04a53ae7cbbbedbea339
DHRUVI THUMMAR <dhruvithummar912@gmail.com>
2026-09-15 12:49:29 +0530

    Add files via upload

---
+++ unit-3/p2A.c
@@ -0,0 +1,37 @@
+  //A insert a node at the starting of the list
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ struct Node {
+     int data;
+     struct Node *next;
+ };
+
+ int main() {
+     struct Node *head = NULL;
+     struct Node *newNode;
+
+     newNode = (struct Node *)malloc(sizeof(struct Node));
+
+     printf("Enter data: ");
+     scanf("%d", &newNode->data);
+
+     newNode->next = head;
+     head = newNode;
+
+     printf("Linked List: ");
+     struct Node *temp = head;
+
+     while (temp != NULL) {
+         printf("%d -> ", temp->data);
+         temp = temp->next;
+     }
+
+     printf("NULL\n");
+
+     return 0;
+ }
+
+
+
+
